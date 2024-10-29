d77236fd1f65b197583e97e9fa9b0cf3da31cc52
nobcha <80092470+Nobcha@users.noreply.github.com>
2024-10-29 19:35:48 +0900

    Update Si5351_GPS-kpa.ino
    GPS corrected VFO

--- Si5351_GPS-kpa.ino
+++ Si5351_GPS-kpa.ino
@@ -1,25 +1,12 @@
-// 2024.10.28 
-// Ported to R909-VFO
-//  LCD pin
-//  RE pin D3, D4, A0
-//  przycisk is assigned as RE-SW  L#32, L#78, L#105, L#189
-//  EN & LCD B.L  L#45 L#88  < LDO_enable
-//  Changed GPS port for SoftwareSerial  L#28 L#99 L#582
-//  Replace CLK0:VFO OUT with CLK1:25MHz  L#96, 102, 103, 104, 105, 561
-//  softwareserial confricts  RE interrupt goes to reset
-//  D5:2.5MHz  softwareserial 
+// GPS_reference_oscylator Si5351_GPS_EP12a_kpa.ino
+// Please refer GITHUB https://github.com/Nobcha/R909-VFO-GPS/
+// Change to R909-VFO assignment by nobcha
+//  LCD pin, EN & LCD B.L
+//  RE pin, przycisk is assigned as RE-SW 
+//  Changed GPS port for SoftwareSerial 
+//  Replace CLK0:VFO OUT with CLK1:2.5MHz
 
-//　2024.10.29　このスケッチの出所GITHUB https://github.com/csqwdy/gps-calibration-5351
-//  動作不安定　RE回すとリセットする。softwareserialは問題なしと思う。
-//  割り込みフラグ設定に問題ありと推定。
-
-//2021-12-23
-//Add control for GPS power supply, so that you can easily use USB to burn firmware
-//Modify the frequency range to 1KHz-200MHz
-
-//2022-03-06
-//Modify the encoder to work in interrupt mode, the encoder becomes more useful
-
+// Original Si5351_GPS_EP12a.ino
 // include the library code:
 #include <TinyGPS++.h>
 #include <LiquidCrystal.h>
@@ -31,39 +18,33 @@
 #include <si5351.h>
 #include <EEPROMex.h>
 
-// #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>         // Changed GPS port for SoftwareSerial 
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
+
 Si5351 si5351;
 
-//int rx=6,tx=17;
-//SoftwareSerial ss(rx,tx);
-
-#define control	                 2    // 1=button ,2=EP11 ,3=EP12
+int rx=6,tx=17;
+SoftwareSerial ss(rx,tx);           // Changed GPS port for SoftwareSerial 
 
 // Set up MCU pins
-#define LDO_Enable               7    // LCD back light & GPS Enable <6
 #define ppsPin                   2
-#define przycisk                A0    // <A2
-#define sw_up					           4    // <A1
-#define sw_down					         3    // <A0
-#define RS                       8    // <7
-#define E                        9    // <8
-#define DB4                     10    // < 9
-#define DB5                     11    // <10
-#define DB6                     12    // <11
-#define DB7                     13    // <12
-#define Freq2					1000000000ULL
+#define encoderPinA              3  // <A1
+#define encoderPinB              4  // <A0
+#define clk1_2500                5  // 2.5MHz
+#define przycisk                A0  // < RE-SW
+// 4 switches service by A2 port ADC  SW1:<80, SW2:<250, SW3:<430, SW4:<600
 
-volatile byte seqA = 0;
-volatile byte seqB = 0;
-volatile byte cnt1 = 0;
-volatile byte cnt2 = 0;
-volatile boolean right = false;
-volatile boolean left = false;
-volatile boolean button = false;
+#define RS                       8  // <7
+#define E                        9  // <8
+#define DB4                     10  // < 9
+#define DB5                     11  // <10
+#define DB6                     12  // <11
+#define DB7                     13  // <12
 
+// GPS/EN & LCD back light on
+#define EN                       7  // 
 
 LiquidCrystal lcd(RS, E, DB4, DB5, DB6, DB7);
 
@@ -88,91 +69,68 @@ byte encoderOLD, menu = 0, band = 1, f_step = 1;
 boolean time_enable = true;
 unsigned long pps_correct;
 byte pps_valid = 1;
-byte correct_byte;
-float stab_float;
 //*************************************************************************************
 //                                    SETUP
 //*************************************************************************************
 void setup()
 {
-	
-  pinMode(LDO_Enable, OUTPUT);          // GPS Power control pin enable
-  digitalWrite(LDO_Enable, HIGH);       // GPS The power control pin outputs a high level
-//  pinMode(encoderPinA, INPUT);        // Set up rotary encoder
+  pinMode(encoderPinA, INPUT_PULLUP);                   // Set up rotary encoder
 //  digitalWrite(encoderPinA, HIGH);
-//  pinMode(encoderPinB, INPUT);
+  pinMode(encoderPinB, INPUT_PULLUP);
 //  digitalWrite(encoderPinB, HIGH);
-  pinMode(sw_up, INPUT);                // Set up rotary encoder
-  digitalWrite(sw_up, HIGH);
-  pinMode(sw_down, INPUT);
-  digitalWrite(sw_down, HIGH);
+
+  pinMode(przycisk, INPUT_PULLUP);               // Set up push buttons
   
-  pinMode(przycisk, INPUT);             // Set up push buttons
-  digitalWrite(przycisk, HIGH);
-
-//
-//  PCICR =  0b00000010;                  // 1. PCIE1: Pin Change Interrupt Enable 1
-//  PCMSK1 = 0b00000111;                  // Enable Pin Change Interrupt for A0, A1, A2
-//
-  PCICR =  0b00000110;                  // 1. PCIE2: Pin Change Interrupt Enable D0-D7
-  PCMSK2 = 0b00001100;                  // Enable Pin Change Interrupt for D3, D4
-  PCMSK1 = 0b00000001;                  // Enable Pin Change Interrupt for A0
-
   TCCR1B = 0;                                    //Disable Timer5 during setup
   TCCR1A = 0;                                    //Reset
   TCNT1  = 0;                                    //Reset counter to zero
   TIFR1  = 1;                                    //Reset overflow
   TIMSK1 = 1;                                    //Turn on overflow flag
-  pinMode(ppsPin, INPUT);                        // Inititalize GPS 1pps input as D2
-  digitalWrite(ppsPin, HIGH);
 
+//  pinMode(ppsPin, INPUT_PULLUP);                 // Inititalize GPS 1pps input
+//  pinMode(clk1_2500, INPUT);
 
+  pinMode(EN, OUTPUT);                           // Turn on GPS WE
+  digitalWrite(EN, HIGH);                        // Also turn on LCD back light
+  
   lcd.begin(16, 2);
-  lcd.display();
-  if (digitalRead(przycisk) == 0) {     // If RE-SW on when starting up
-	  lcd.print("Initialization");
-	  EEPROM.writeLong(1 * 4, 10000000);
-	  EEPROM.writeLong(2 * 4, 16700000);
-	  EEPROM.writeLong(3 * 4, 37200000);
-	  EEPROM.writeLong(4 * 4, 64800000);
-	  EEPROM.writeLong(5 * 4, 145000000);
-	  EEPROM.writeByte(82, 1);
-	  EEPROM.writeInt(80, 1);
-	  EEPROM.writeLong(90, 0);
-	  EEPROM.writeByte(99, 1);
-	  delay(3000);
-  }
-	band = EEPROM.readByte(82);
-	Freq = EEPROM.readLong(band * 4);
-	zone = EEPROM.readInt(80);
-	correct_byte = EEPROM.readByte(99);
-	correction = EEPROM.readLong(90);
-	
   si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
-  si5351.drive_strength(SI5351_CLK1, SI5351_DRIVE_8MA);
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_6MA);
+  si5351.drive_strength(SI5351_CLK1, SI5351_DRIVE_6MA); // add
 
   Serial.begin(9600);
+  ss.begin(9600);                 // GPS's default communication speed  
+  
 
-  // Set CLK0 to output 2,5MHz
-  si5351.set_ms_source(SI5351_CLK0, SI5351_PLLA);
-  si5351.set_freq(250000000ULL, SI5351_CLK0);
-  si5351.set_ms_source(SI5351_CLK1, SI5351_PLLB);
-  si5351.set_ms_source(SI5351_CLK2, SI5351_PLLA);
-  si5351.set_freq(Freq * SI5351_FREQ_MULT, SI5351_CLK1);
-  si5351.set_freq(Freq2, SI5351_CLK2);
-  correct_si5351a();
+  si5351.set_ms_source(SI5351_CLK0, SI5351_PLLA);         // B A
+  si5351.set_freq(Freq * SI5351_FREQ_MULT, SI5351_CLK0);
+  
+    // Set CLK1 to output 2,5MHz
+  si5351.set_ms_source(SI5351_CLK1, SI5351_PLLB);        // A B
+  si5351.set_freq( 2500000 * SI5351_FREQ_MULT, SI5351_CLK1);
+  
   si5351.update_status();
 
+  lcd.display();
+  if (digitalRead(przycisk) == 0) {               // RE-SW key pressed
+    lcd.print("Initialization");
+    EEPROM.writeLong(1 * 4, 10000000);
+    EEPROM.writeLong(2 * 4, 16700000);
+    EEPROM.writeLong(3 * 4, 37200000);
+    EEPROM.writeLong(4 * 4, 64800000);
+    EEPROM.writeLong(5 * 4, 145000000);
+    delay(3000);
+  }
   lcd.clear();
   lcd.print(" GPS  GENERATOR");
   lcd.setCursor(0, 1);
-  lcd.print("Si5351a by SQ1GU");
-  delay(1000);
+  lcd.print("by SQ1GU&JA3KPA");
+  delay(3000);
   lcd.clear();
   lcd.setCursor(0, 1);
   lcd.print("Waiting for GPS");
 
-  GPSproces(2000);
+  GPSproces(6000);
 
   if (millis() > 5000 && gps.charsProcessed() < 10) {
     lcd.home();
@@ -183,36 +141,32 @@ void setup()
     GPSstatus = false;
   }
   lcd.clear();
-  
-  
-  // if GPS connected then...
-  if (GPSstatus == true) {
+  if (GPSstatus == true) {                     // GPS flag true
     lcd.print("Waiting for SAT");
     time_on_lcd();
     sat_on_lcd();
     do {
       GPSproces(1000);
-    } while (gps.satellites.value() == 0);
+    } while (gps.satellites.value() == 0);     // Wait GPS being active
 
-    hour = gps.time.hour() + zone;
+    hour = gps.time.hour() + zone;             // when becoming active, display time
     minute = gps.time.minute();
     second = gps.time.second();
     time_on_lcd();
     sat_on_lcd();
-    attachInterrupt(0, PPSinterrupt, RISING);
+
+    attachInterrupt(0, PPSinterrupt, RISING);  // D2 INT enable
     TCCR1B = 0;
     tcount = 0;
     mult = 0;
     validGPSflag = 1;
+    
   }
- 
-  
   freq_on_lcd();
   lcd.print(" kHz");
   sat_on_lcd();
   time_on_lcd();
 }
-
 //***************************************************************************************
 //                                         LOOP
 //***************************************************************************************
@@ -226,15 +180,15 @@ void loop()
     GPSproces(0);
   }
   if (gps.time.isUpdated()) {
-    hour = (gps.time.hour() + zone) % 24;
-	if(hour<0) hour = hour * -1;
+    hour = gps.time.hour() + zone;
     minute = gps.time.minute();
     second = gps.time.second();
   }
+  
   if (gps.satellites.isUpdated() && menu == 0) {
     sat_on_lcd();
   }
-
+  
   if (new_freq == 1) {
     correct_si5351a();
     new_freq = 0;
@@ -246,8 +200,10 @@ void loop()
     freq_on_lcd();
     new_freq = 0;
   }
+
+// 4 switches service by RE-SW
   if (digitalRead(przycisk) == 0) {
-    delay(10);
+    delay(30);
     if (digitalRead(przycisk) == 0) {
       menu++;
       if (menu > 5) menu = 0;
@@ -275,9 +231,10 @@ void loop()
           timezone_on_lcd();
           break;
       }
-      delay(200);
+      delay(300);
     }
   }
+
 
 
 
@@ -289,69 +246,25 @@ void loop()
     lcd.print(" ");
 
   }
-  
-  //ENCread();
-
-  //The encoder is changed to interrupt mode
-  if (left) {
-    left = false;
-    change_down();     
-  }
-
-  if (right) {
-    right = false;
-    change_up();     
-  }
-
+  ENCread();
 }
-/*
+
 //**************************************************************************************
 //                       INTERRUPT  ENC
 //**************************************************************************************
-void ENCread(){
-	if(control == 1){
-		if(digitalRead(sw_up) == 0){
-			delay(200);
-			if (digitalRead(sw_up) == 0) change_up(); delay(200);
-		}
-		if(digitalRead(sw_down) == 0){
-			delay(200);
-			if (digitalRead(sw_down) == 0) change_down(); delay(200);			
-		}
-	}
-	
-  if (control == 2){
-    if (digitalRead(encoderPinA) == 0) {
-      delay(2);
-      if (digitalRead(encoderPinA) == 0) {
-        if (digitalRead(encoderPinB) == 0) change_up();
-        else change_down();  
-        delay(40); 
-      }
-    }
-  } 
-  
-	if(control == 3){
-		if (digitalRead(encoderPinA) != encoderOLD) {
-		delay(4);
-			if (digitalRead(encoderPinA) != encoderOLD) {
-				encoderOLD = digitalRead(encoderPinA);
-				if (digitalRead(encoderPinA) == digitalRead(encoderPinB)) change_up();
-				else change_down();
-       delay(50);
-			}
-		}
-	}
-}
-*/
-//*******************************************
-void change_up(){
+void ENCread()
+{
+  if (digitalRead(encoderPinA) != encoderOLD) {
+    delay(5);
+    if (digitalRead(encoderPinA) != encoderOLD) {
+      encoderOLD = digitalRead(encoderPinA);
+      if (digitalRead(encoderPinA) == digitalRead(encoderPinB)) {
+
         switch (menu) {
           case 1: {
               band++;
               if (band > 5) band = 5;
-			  EEPROM.writeByte(82, band);
-			  band_on_lcd();
+              band_on_lcd();
             }
             break;
           case 2: {
@@ -362,27 +275,26 @@ void change_up(){
             break;
           case 3: {
               Freq += freq_step;
-              if (Freq > 200000000) Freq -= freq_step;
+              if (Freq > 160000000) Freq -= freq_step;
               new_freq = 2;
             }
             break;
           case 5: {
               zone++;
-              if (zone > 12)zone = 12;
+              if (zone > 6)zone = 5;
               EEPROM.writeInt(80, zone);
               timezone_on_lcd();
             }
             break;
         }
-}
-void change_down(){
+      }
+      else {
         switch (menu) {
           case 1: {
               band--;
               if (band == 0) band = 1;
-              EEPROM.writeByte(82, band);
-			  band_on_lcd();
-			  }
+              band_on_lcd();
+            }
             break;
           case 2: {
               f_step--;
@@ -392,29 +304,32 @@ void change_down(){
             break;
           case 3: {
               Freq -= freq_step;
-              if (Freq > 200000000 || Freq < 1000) Freq += freq_step;
+              if (Freq > 160000000 || Freq < 1000) Freq += freq_step;
               new_freq = 2;
             }
             break;
           case 5: {
               zone--;
-              if (zone < -12)zone = -12;
+              if (zone < -6)zone = -5;
               EEPROM.writeInt(80, zone);
               timezone_on_lcd();
             }
             break;
-        }   
+        }
+      }
+      delay(60);
+    }
+  }
 }
-
 //**************************************************************************************
 //                       INTERRUPT  1PPS
 //**************************************************************************************
-//******************************************************************
 void PPSinterrupt()
 {
+
   tcount++;
   stab_count--;
-  if (tcount == 4)                               // Start counting the 2.5 MHz signal from Si5351A CLK0
+  if (tcount == 4)                               // Start counting the 2.5 MHz signal from Si5351A CLK1
   {
     TCCR1B = 7;                                  //Clock on rising edge of pin 5
     // loop();
@@ -424,20 +339,19 @@ void PPSinterrupt()
     TCCR1B = 0;                                  //Turn off counter
     if (pps_valid == 1) {
       XtalFreq_old = XtalFreq;
-      XtalFreq = mult * 0x10000 + TCNT1;           //Calculate correction factor
+      XtalFreq = mult * 0x10000 + TCNT1;         //Calculate correction factor
       new_freq = 1;
-    }
+  }
     TCNT1 = 0;                                   //Reset count to zero
     mult = 0;
     tcount = 0;                                  //Reset the seconds counter
     pps_valid = 1;
-    Serial.begin(9600);
+
     stab_count = 44;
-	correct_freq();
-    if (menu == 4)stab_on_lcd();
-  }
-  if (validGPSflag == 1)                      //Start the UTC timekeeping process
-  {
+  if (menu == 4)stab_on_lcd();
+}
+if (validGPSflag == 1)                           //Start the UTC timekeeping process
+{
     second++;
     if (second == 60)                            //Set time using GPS NMEA data
     {
@@ -457,9 +371,11 @@ void PPSinterrupt()
     if (stab_count < 10) lcd.print(" ");
     lcd.print(stab_count);
   }
+  
 }
 //*******************************************************************************
-// Timer 1 overflow intrrupt vector.
+//                   Timer 1 overflow intrrupt vector.
+//*******************************************************************************
 ISR(TIMER1_OVF_vect)
 {
   mult++;                                          //Increment multiplier
@@ -478,49 +394,33 @@ void timezone_on_lcd()
   lcd.print(" < > ");
 }
 //********************************************************************************
-//                                STAB on LCD  stabilnośc częstotliwości
+//                                STAB on LCD  Frequency stability
 //********************************************************************************
 void stab_on_lcd() {
-
+  float stab_float;
+  long pomocna;
+  time_enable = false;
   lcd.setCursor(7, 1);
   lcd.print("         ");
   lcd.setCursor(0, 1);
   lcd.print("Fstab ");
 
-  lcd.print(stab_float);
-  lcd.print("Hz");
-}
-//********************************************************************************
-//   correct ferquency
-//********************************************************************************
-void correct_freq()
-{
-  long pomocna;
-  time_enable = false;
-
   stab = XtalFreq - 100000000;
   stab = stab * 10 ;
   if (stab > 100 || stab < -100) {
-	  correction = correction + stab;
+    correction = correction + stab;
   }
   else if (stab > 20 || stab < -20) {
-	  correction = correction + stab / 2;
+    correction = correction + stab / 2;
   }
   else correction = correction + stab / 4;
-  
-  if(stab < 5 || stab > -5) {
-	  if(correct_byte != 0){
-		  EEPROM.writeLong(90, correction);
-		  EEPROM.writeByte(99, 0);
-	  }
-  }
-  
   pomocna = (10000 / (Freq / 1000000));
   stab = stab * 100;
   stab = stab / pomocna;
   stab_float = float(stab);
   stab_float = stab_float / 10;
-	
+  lcd.print(stab_float);
+  lcd.print("Hz");
 }
 //********************************************************************************
 //                                FREQ_2 on LCD <>
@@ -581,8 +481,7 @@ void time_on_lcd()
 {
   char sz[32];
   sprintf(sz, "%02d:%02d:%02d ", hour, minute, second);
-  lcd.setCursor(7, 1);
-  lcd.print(" ");
+  lcd.setCursor(8, 1);
   lcd.print(sz);
 }
 //********************************************************************************
@@ -594,8 +493,9 @@ void sat_on_lcd()
   lcd.setCursor(0, 1);
   lcd.print("SAT ");
   lcd.print(gps.satellites.value());
-  lcd.print("  ");
-  time_enable = true;
+  lcd.print("   ");
+
+   time_enable = true;  
 }
 
 //*********************************************************************************
@@ -673,7 +573,7 @@ void freq_on_lcd() {
 //********************************************************************
 void update_si5351a()
 {
-  si5351.set_freq(Freq * SI5351_FREQ_MULT, SI5351_CLK1);
+  si5351.set_freq(Freq * SI5351_FREQ_MULT, SI5351_CLK0);
 
 }
 //********************************************************************
@@ -687,57 +587,15 @@ void correct_si5351a()
 
 }
 //*********************************************************************
-//                    Data from GPS
+//                    Data read from GPS
 //**********************************************************************
 static void GPSproces(unsigned long ms)
 {
   unsigned long start = millis();
   do
   {
-    while (Serial.available())
-      gps.encode(Serial.read());
+    while (ss.available())           // Software Serial
+      gps.encode(ss.read());         // Changed GPS port for SoftwareSerial  
   } while (millis() - start < ms);
 }
 //*********************************************************************
-
-
-ISR (PCINT1_vect) {
-
-// If interrupt is triggered by the button
-  if (!digitalRead(przycisk)) {
-    
-    button = true;}
-
-// Else if interrupt is triggered by encoder signals
-  else {
-    
-    // Read A and B signals
-    //boolean A_val = digitalRead(encoderPinB);
-    //boolean B_val = digitalRead(encoderPinA);
-    boolean A_val = digitalRead(sw_down);
-    boolean B_val = digitalRead(sw_up);
-    
-    // Record the A and B signals in seperate sequences
-    seqA <<= 1;
-    seqA |= A_val;
-    
-    seqB <<= 1;
-    seqB |= B_val;
-    
-    // Mask the MSB four bits
-    seqA &= 0b00001111;
-    seqB &= 0b00001111;
-    
-    // Compare the recorded sequence with the expected sequence
-    if (seqA == 0b00001001 && seqB == 0b00000011) {
-      cnt1++;
-      left = true;
-      }
-     
-    if (seqA == 0b00000011 && seqB == 0b00001001) {
-      cnt2++;
-      right = true;
-      }
-  }
-
-}  
